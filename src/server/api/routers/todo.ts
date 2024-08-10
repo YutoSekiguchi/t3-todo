817683e784586733a3d108753550f86cb6c214b9
YutoSekiguchi <yuutosekiguchi@gmail.com>
2024-08-10 23:40:13 +0900

    first commit

---
+++ src/server/api/routers/todo.ts
@@ -0,0 +1,65 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { todoInput } from "~/types";
+
+export const todoRouter = createTRPCRouter({
+  all: protectedProcedure.query(async ({ ctx }) => {
+    const todos = await ctx.db.todo.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+    console.log(todos.map(({ id, text, done }) => ({ id, text, done })));
+    return [
+      {
+        id: "fake",
+        title: "fake text",
+        done: false,
+      },
+      {
+        id: "fake2",
+        title: "fake text 2",
+        done: true,
+      },
+    ];
+  }),
+
+  create: protectedProcedure
+    .input(todoInput)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.todo.create({
+        data: {
+          text: input,
+          user: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.todo.delete({
+        where: {
+          id: input,
+        },
+      });
+    }),
+
+  toggle: protectedProcedure
+    .input(z.object({ id: z.string(), done: z.boolean() }))
+    .mutation(async ({ ctx, input: { id, done } }) => {
+      return ctx.db.todo.update({
+        where: {
+          id: id,
+        },
+        data: {
+          done: done,
+        },
+      });
+    }),
+});
---
+++ src/server/auth.ts
@@ -0,0 +1,77 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import {
+  getServerSession,
+  type DefaultSession,
+  type NextAuthOptions,
+} from "next-auth";
+import { type Adapter } from "next-auth/adapters";
+import EmailProvider from "next-auth/providers/email";
+
+// import { env } from "~/env";
+import { db } from "~/server/db";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER ?? "http://localhost:3000",
+        port: 587,
+        auth: {
+          user: "apikey",
+          pass: process.env.EMAIL_PASSWORD ?? "",
+        },
+      },
+      from: process.env.EMAIL_FROM ?? "default@default.com",
+      ...(process.env.NODE_ENV !== "production"
+        ? {
+            sendVerificationRequest({ url }) {
+              console.log("LOGIN LINK", url);
+            },
+          }
+        : {}),
+    }),
+  ],
+};
+
+/**
+ * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
+ *
+ * @see https://next-auth.js.org/configuration/nextjs
+ */
+export const getServerAuthSession = () => getServerSession(authOptions);
